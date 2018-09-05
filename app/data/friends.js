9b70b08fd97842e67bbc2f0aa2e80103f9338e3f
Abraham Ibarra <aibarra988@gmail.com>
2018-09-05 03:23:29 -0500

    I think im done

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+let friends = [
+    {
+        name: "Goofy",
+        image_url: "https://vignette.wikia.nocookie.net/disney/images/2/27/Goofy_transparent.png",
+        answers: [1, 3, 4, 3],
+        about: "Hy-uch! That's me! I'm always trying to be positive so you'd better be ready for that!"
+    },
+    {
+        name: "Kramer",
+        image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/Cosmo_Kramer.jpg/250px-Cosmo_Kramer.jpg",
+        answers: [1, 2 , 3, 4],
+        about: "I'm out there, Jerry. Loving every minute of it!"
+    },
+    {
+        name: "Scorpion",
+        image_url: "https://i.ytimg.com/vi/L3w75KAOhdw/hqdefault.jpg",
+        answers: [1, 1, 1, 4],
+        about: "Get over here, new pal!"
+    },
+    {
+        name: "A Cucumber",
+        image_url: "https://www.creemmagazine.com/wp-content/uploads/2014/09/how-many-calories-in-cucumber.jpg",
+        answers: [4, 4, 4, 4],
+        about: "*sits silently*"
+    },
+    {
+        name: "poop",
+        image_url: "http://www.erabaru.com.my/wp-content/uploads/2018/02/19231360-14212960-1copy-0-1517399758-1517399788-5669-1-1517399788-650-6f9081b3ae-1517462612.jpg",
+        answers: [1, 1, 4, 3],
+        about: "AAHHH YEAH!"
+    },
+    {
+        name: "Dr. Brule",
+        image_url: "https://pbs.twimg.com/profile_images/723705967649652736/YPsoCbg4_400x400.jpg",
+        answers: [2, 2, 3, 4],
+        about: "I was gonna invent the skrateboard but I already have 500 of them."
+    },
+    {
+        name: "Kevin",
+        image_url: "https://s3.amazonaws.com/media.eremedia.com/uploads/2017/08/ha.jpg",
+        answers: [4, 1, 1, 1],
+        about: "I'm clearly a sociopath"
+    },
+    {
+        name: "Tyrone Biggums",
+        image_url: "https://memegenerator.net/img/images/10817547.jpg",
+        answers: [1, 1, 1, 1],
+        about: "Y'all got any more of the lines of code?"
+    },
+    {
+        name: "Mr. Poopybutthole",
+        image_url: "https://pre00.deviantart.net/5598/th/pre/f/2017/077/7/1/mr_poopybuthole_by_davey459-db2s6ab.png",
+        answers: [1, 3, 4, 4],
+        about: "Hey you're not in any of my pictures!"
+    }
+];
+
+module.exports = friends;
--- app/routing/api.js
+++ app/routing/api.js
@@ -3,17 +3,38 @@ const path = require('path');
 
 const router = express.Router();
 
-const findMatch = req => {
-    
-};
+let friends = require('../data/friends.js');
 
 router.get('/api/image', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/image.jpg"));
 });
 
 router.post('/api/survey', (req, res) => {
-    console.log(req.body);
-    res.status(200).end();
+    // Create newFriend object
+    var newFriend = req.body;
+
+    // Create array that holds 'index' and 'diff from newFriend'
+    var scoreDiffs = [];
+    for (var j = 0; j < friends.length; j++) {
+        var diff = 0;
+        for (var k = 0; k < friends[j].answers.length; k++) {
+            diff += Math.abs(friends[j].answers[k] - newFriend.answers[k]);
+        };
+        scoreDiffs.push(diff);
+    };
+
+    var matchIndex = scoreDiffs.indexOf(Math.min(...scoreDiffs));
+    var match = friends[matchIndex];
+
+    // Add new friend to array
+    friends.push(newFriend);
+
+    // Send back the match
+    return res.json(match);
+});
+
+router.get('/api/friends', (req, res) => {
+    res.json(friends);
 });
 
 module.exports = router;
